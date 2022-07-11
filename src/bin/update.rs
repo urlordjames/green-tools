ff920764a5039ec7d5f6d3587b96db234bc0cd98
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-10 21:42:51 -0400

    add test implementation of update

--- src/bin/update.rs
+++ src/bin/update.rs
@@ -1,3 +1,5 @@
-fn main() {
-    todo!();
+#[tokio::main]
+async fn main() {
+    let directory = green_lib::Directory::from_url("https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json").await.unwrap();
+    directory.upgrade_game_folder(&std::path::PathBuf::from("test")).await;
 }
