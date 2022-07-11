f3d49bd3e4b6368ee5e0228d3a1e1aabc97829f1
python3-trying-py <paytay2023@gmail.com>
2022-07-10 23:36:24 -0400

    implement update

--- src/bin/update.rs
+++ src/bin/update.rs
@@ -1,5 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(long, default_value = "https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json")]
+    url: url::Url,
+    path: Option<std::path::PathBuf>
+}
+
 #[tokio::main]
 async fn main() {
-    let directory = green_lib::Directory::from_url("https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json").await.unwrap();
-    directory.upgrade_game_folder(&std::path::PathBuf::from("test")).await;
+    let args = Args::parse();
+    let directory = green_lib::Directory::from_url(args.url).await.expect("invalid manifest, you fucking dumbass");
+    let path = match args.path {
+        Some(path) => path,
+        None => green_lib::util::minecraft_path()
+    };
+    directory.upgrade_game_folder(&path).await;
 }
