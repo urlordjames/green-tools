120ad84d3a2510856c3f7c10c848b628e8e2de8b
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-13 17:11:39 -0400

    change readme and do a little refactoring on green-update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # green-tools
 tools to assist with [green-updater](https://github.com/urlordjames/green-updater) using [green-lib](https://github.com/urlordjames/green-lib)
 
-## manifest
+## green-manifest
 generates a manifest for the specified file path and URL to stdout
 
-## update (unimplemented)
+## green-update
 a headless version of [green-updater](https://github.com/urlordjames/green-updater) intended for use in servers, automation, or other platforms where GTK is an issue
--- src/bin/green-update.rs
+++ src/bin/green-update.rs
@@ -4,16 +4,19 @@ use clap::Parser;
 struct Args {
     #[clap(long, default_value = "https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json")]
     url: url::Url,
+
     path: Option<std::path::PathBuf>
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let directory = green_lib::Directory::from_url(args.url).await.expect("invalid manifest, you fucking dumbass");
+    let directory = green_lib::Directory::from_url(args.url).await.expect("invalid manifest");
+
     let path = match args.path {
         Some(path) => path,
         None => green_lib::util::minecraft_path()
     };
+
     directory.upgrade_game_folder(&path).await;
 }
