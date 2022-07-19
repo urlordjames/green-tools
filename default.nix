1f789bb57100db9a3bf1fa935884c9583bffd191
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-18 21:37:34 -0400

    update default.nix checksum

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ in pkgs.rustPlatform.buildRustPackage {
 	cargoLock = {
 		lockFile = ./Cargo.lock;
 		outputHashes = {
-			"green-lib-0.1.1" = "sha256-DvS9i4jZq65ADTp/zBKLq6ytJgNC12Bfvm9teLrw0bM=";
+			"green-lib-0.1.2" = "sha256-XaGbHSFMZI7WOSgycjNHFHrbWBkFkRXJjO5aRSNCPhE=";
 		};
 	};
 }
