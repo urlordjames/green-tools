963bb822ba8b2fc14adecb823007e79fe4e87044
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-05-31 19:33:54 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+		flake-utils.url = "github:numtide/flake-utils";
+
+		crane = {
+			url = "github:ipetkov/crane";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, flake-utils, crane }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs {
+				inherit system;
+			};
+			craneLib = crane.lib.${system};
+			green-tools = craneLib.buildPackage {
+				src = craneLib.cleanCargoSource (craneLib.path ./.);
+			}; in {
+				devShell = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						cargo
+					];
+				};
+
+				packages.default = green-tools;
+			}
+		);
+}
